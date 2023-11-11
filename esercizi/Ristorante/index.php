5218e4efd7e83ab31afd34a0b637e1e24dee3ab7
DamianoBernardi <146942514+DamianoBernardi@users.noreply.github.com>
2023-11-11 11:45:09 +0000

    riniminato

---
+++ esercizi/Ristorante/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <h1>PIZZERIA MAMMA MIA</h1>
+            <h4>La pizzeria migliore di tutta l bella italia!</h4>
+        </header>
+        
+        <?php
+        $pizze = array ("margherita", "capricciosa", "diavola", "patatosa", "prosciutto e funghi", "viennese", )
+        ?>
+    </body>
+</html>
