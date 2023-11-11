ac62cbde0464291b40517110a0267137ccf2cd24
DamianoBernardi <146942514+DamianoBernardi@users.noreply.github.com>
2023-11-11 11:57:12 +0000

    modifica1

--- esercizi/Ristorante/index.php
+++ esercizi/Ristorante/index.php
@@ -10,11 +10,15 @@
     <body>
         <header>
             <h1>PIZZERIA MAMMA MIA</h1>
-            <h4>La pizzeria migliore di tutta l bella italia!</h4>
+            <h6>La pizzeria migliore di tutta l bella italia!</h6>
         </header>
         
         <?php
-        $pizze = array ("margherita", "capricciosa", "diavola", "patatosa", "prosciutto e funghi", "viennese", )
+        $pizze = array ("margherita", "capricciosa", "diavola", "patatosa", "prosciutto e funghi", "viennese");
+        foreach( $pizze as $variabile)
+        {
+            echo "<h2> $variabile </h2>";
+        }
         ?>
     </body>
 </html>
