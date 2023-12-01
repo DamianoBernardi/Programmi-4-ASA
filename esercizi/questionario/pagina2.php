47063cfd681cbaec2404d65bc883b52564c40d63
DamianoBernardi <146942514+DamianoBernardi@users.noreply.github.com>
2023-12-01 17:31:58 +0000

    modifica12

--- esercizi/questionario/pagina2.php
+++ esercizi/questionario/pagina2.php
@@ -9,7 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
-  <body style="color: <?=$_GET['sfondo'] ?>">
+  <body style="color: <?=$_GET['Colore_sfondo'] ?>">
     <header class="bd-header bg-dark py-3 d-flex align-items-stretch border-bottom border-dark">
         <div class="container-fluid d-flex align-items-center">
             <h1 class="d-flex align-items-center fs-4 text-white mb-0">
